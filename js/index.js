c1c349ad065560105ce4139cca693256aff1afac
kimmiechoo <krzelaya@gmail.com>
2022-02-22 18:37:28 -0700

    Updating code for images

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 console.log("Hello world!");
 $(document).ready(function(){
     // Add smooth scrolling to all links
-    $("primary-button").on('click', function(event) {
+    $(".primary-button").on('click', function(event) {
   
       // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "myWork") {
+      if (this.hash !== "") {
         // Prevent default anchor click behavior
         event.preventDefault();
   
         // Store hash
         var hash = this.hash;
-  
+  console.log(hash)
         $('html, body').animate({
           scrollTop: $(hash).offset().top
         }, 800, function(){
