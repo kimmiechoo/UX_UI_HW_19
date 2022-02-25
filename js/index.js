d80e14b12835addcf4dffad45f4c0c7bba8cd1fe
kimmiechoo <krzelaya@gmail.com>
2022-02-24 20:12:45 -0700

    JQuery update
    Updated code to add smooth scroll for JS, targeted scroll buttons,

--- js/index.js
+++ js/index.js
@@ -1,23 +1,11 @@
 console.log("Hello world!");
 $(document).ready(function(){
     // Add smooth scrolling to all links
-    $(".primary-button").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  console.log(hash)
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-     
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
+    $(".scroll").on('click', function(e) {
+e.preventDefault()
+var myWork= $(this).attr("href")
+$("html, body").animate({
+  "scrollTop": $(myWork).offset().top
+}, 800)
+})
         });
-      } // End if
-    });
-  });
